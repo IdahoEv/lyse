374f834939562407469b2648478d6df6fc195827
Evan Dorn <evan@lrdesign.com>
2014-02-25 10:16:38 -0800

    tail recursive version of factorial.

--- 07_recursion/recursive.erl
+++ 07_recursion/recursive.erl
@@ -1,9 +1,11 @@
 -module(recursive).
 -export([fac/1, fib/1, len/1]).
+-export([tail_fac/1]). %, fib/1, len/1]).
 
 fac(N) when N == 0 -> 1;
 fac(N) when N > 0  -> N * fac(N-1).
 
+% Really dumb implementation of fib.
 fib(N) when N == 0 -> 1;
 fib(N) when N == 1 -> 1;
 fib(N) when N > 1  -> fib(N-1) + fib(N-2).
@@ -11,5 +13,12 @@ fib(N) when N > 1  -> fib(N-1) + fib(N-2).
 len([])  -> 0;
 len([_]) -> 1;
 len([_|T]) -> 1 + len(T).
+
+
+% tail recursive versions
+tail_fac(N)      -> tail_fac(N, 1).
+tail_fac(0, Acc) -> Acc;
+tail_fac(N, Acc) -> tail_fac(N-1, Acc * N).
+
 
 
